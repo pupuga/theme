2994fbe7cb42f74c31c771337a46fb6e2e1f2208
shandor <mvshandor@gmail.com>
2021-04-23 10:59:08 +0300

    upgraded -> npm-modules

--- header.php
+++ header.php
@@ -1,24 +1,27 @@
 <?php global $post; ?>
 <!doctype html>
-<html <?php language_attributes() ?>>
+<html <?php language_attributes(); ?>>
 <head>
     <?php Pupuga\Libs\Files\Files::getTemplate(DIR_TEMPLATES . 'meta', true) ?>
     <?php wp_head() ?>
     <?php //Pupuga\Libs\Files\Files::getCss(DIR_ASSETS . 'skeleton.css', true) ?>
     <script>let globalData = <?php echo Pupuga\Core\Base\Common::app()->getJs() ?></script>
 </head>
-<body class="<?php if ($frontPage = is_front_page()) : ?>home-page<?php else : ?>not-home-page<?php endif ?>">
-    <header class="general__header <?php if ($frontPage) : ?>general__header--home<?php else : ?>general__header--not-home<?php endif ?>">
-       <div class="block-columns">
-           <div class="block-columns__left">
-               <?php Pupuga\Libs\Files\Files::getTemplate(DIR_TEMPLATES . 'meta', true, array('class' => 'general__logo')) ?>
-	   </div>
-           <div class="block-columns__right">
-               <nav class="menu menu--line">
-	           <ul>
-                       <?php Pupuga\Core\Load\Menu::app()->menuStandard('Main')->action() ?>
-	           </ul>
-	       </nav>
-           </div>
-       </div>
-    </header>
+<body <?php body_class() ?>>
+<header class="general__header">
+    <div class="block-columns">
+        <div class="block-columns__left">
+            <?php Pupuga\Libs\Files\Files::getTemplate(DIR_TEMPLATES . 'logo', true, array('class' => 'general__logo')) ?>
+        </div>
+        <div class="block-columns__right">
+            <nav class="lang-list">
+                <?php echo Pupuga\Custom\Lang\Languages::app()->getList('select') ?>
+            </nav>
+            <nav class="menu menu--line">
+                <ul>
+                    <?php Pupuga\Core\Load\Menu::app()->menuStandard('Main')->action() ?>
+                </ul>
+            </nav>
+        </div>
+    </div>
+</header>
